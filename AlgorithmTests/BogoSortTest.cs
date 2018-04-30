711936a5be80f0bf5a5b02b90aa4b9d6536d4acc
D-Poschel <10940956@live.mercer.edu>
2018-04-30 18:17:20 -0400

    BogoBoyo

---
+++ AlgorithmTests/BogoSortTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DankAlgorithms.Algorithms;
+using DankAlgorithms.Utilities;
+
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class BogoSortTest
+    {
+        BogoSort bogo = new DankAlgorithms.Algorithms.BogoSort();
+        [TestMethod]
+        public void TestBogoAccuracy()
+        {
+            int[] testArray = { 2, 3, 5, 1, 4 };
+            int[] sortedArray = bogo.Sort(testArray);
+            Assert.AreEqual(sortedArray[0], 1);
+        }
+    }
+}
