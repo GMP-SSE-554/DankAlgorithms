1d333c20e14320f41232dca7456b1cfd6f801b0e
D-Poschel <10940956@live.mercer.edu>
2018-04-30 17:19:55 -0400

    BogoAndUtils
    added IsSorted() to check if array is sorted

---
+++ DankAlgorithms/Algorithms/BogoSort.cs
@@ -0,0 +1,27 @@
+﻿using DankAlgorithms.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DankAlgorithms.Algorithms
+{
+    class BogoSort
+    {
+        public int[] Sort(int[] inputArray)
+        {
+            int[] temp = inputArray;
+            while (!ArrayUtils.IsSorted(temp))
+                Shuffle(temp);
+            return temp;
+        }
+
+        private int[] Shuffle(int[] inputArray)
+        {
+            Random rnd = new Random();
+            int[] newArray = inputArray.OrderBy(x => rnd.Next()).ToArray();
+            return newArray;
+        }
+    }
+}
