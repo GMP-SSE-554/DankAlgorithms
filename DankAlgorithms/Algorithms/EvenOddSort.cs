4ad71df5ca813209129e30fdbce5ab149e905a72
ToastyTad <toastytadgit@gmail.com>
2018-04-30 21:34:47 -0400

    can now cancel tasks.

--- DankAlgorithms/Algorithms/EvenOddSort.cs
+++ DankAlgorithms/Algorithms/EvenOddSort.cs
@@ -1,23 +1,26 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DankAlgorithms.Algorithms
 {
     public class EvenOddSort
     {
-        CancellationTokenSource _cts;
+        private static CancellationTokenSource _cts;
 
         /// <summary>
-        /// Returns asyncronous sorting task.
+        /// Starts an asyncronous sorting task.
         /// </summary>
         /// <param name="array">The array.</param>
+        /// <param name="cts">The CTS.</param>
         /// <returns></returns>
-        public static Task SortAsync(int[] array)
+        public static Task SortAsync(int[] array, CancellationTokenSource cts)
         {
             return Task.Run(() =>
             {
+                _cts = cts;
                 return Sort(array);
-            });
+            }, cts.Token);
         }
 
         /// <summary>
@@ -29,32 +32,40 @@ namespace DankAlgorithms.Algorithms
             int i, j, tmp, size = array.Length;
             int[] copy = new int[size];
             array.CopyTo(copy, 0);
-            for (i = 0; i < size; i++)
+            try
             {
-                if (i % 2 == 0)
+                for (i = 0; i < size; i++)
                 {
-                    for (j = 1; j < size; j += 2)
+                    _cts?.Token.ThrowIfCancellationRequested();
+                    if (i % 2 == 0)
                     {
-                        if (copy[j - 1] > copy[j])
+                        for (j = 1; j < size; j += 2)
                         {
-                            tmp = copy[j - 1];
-                            copy[j - 1] = copy[j];
-                            copy[j] = tmp;
+                            if (copy[j - 1] > copy[j])
+                            {
+                                tmp = copy[j - 1];
+                                copy[j - 1] = copy[j];
+                                copy[j] = tmp;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (j = 1; j < size - 1; j += 2)
+                        {
+                            if (copy[j] > copy[j + 1])
+                            {
+                                tmp = copy[j + 1];
+                                copy[j + 1] = copy[j];
+                                copy[j] = tmp;
+                            }
                         }
                     }
                 }
-                else
-                {
-                    for (j = 1; j < size - 1; j += 2)
-                    {
-                        if (copy[j] > copy[j + 1])
-                        {
-                            tmp = copy[j + 1];
-                            copy[j + 1] = copy[j];
-                            copy[j] = tmp;
-                        }
-                    }
-                }
+            }
+            catch (OperationCanceledException ex)
+            {
+                Console.WriteLine(ex.Message);
             }
             return copy;
         }
