e81cdc5fb0cb11556ef82d76a2b0f05e8ecfe4cb
SeditiousSnake <tyson31088@gmail.com>
2018-04-30 21:03:46 -0400

    Added a bunch of Parallel Stuff

---
+++ AlgorithmTests/ParallelArrayAdditionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DankAlgorithms.Algorithms;
+
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class ParallelArrayAdditionTest
+    {
+        [TestMethod]
+        public void TestParallelArrayAdditionAccuracy()
+        {
+            int[] ArrayA = { 1, 2, 3 };
+            int[] ArrayB = { 1, 2, 3 };
+
+            int[] OutputArray = ParallelArrayAddition.AddArrays(ArrayA, ArrayB);
+
+            Assert.AreEqual(2, OutputArray[0]);
+            Assert.AreEqual(4, OutputArray[1]);
+            Assert.AreEqual(6, OutputArray[2]);
+        }
+
+    }
+}
