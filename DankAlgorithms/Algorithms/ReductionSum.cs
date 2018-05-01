7d1d8b7b2f121329e1c22831a37f4f95c1edbad4
SeditiousSnake <tyson31088@gmail.com>
2018-04-30 23:22:05 -0400

    Simple fixes

--- DankAlgorithms/Algorithms/ReductionSum.cs
+++ DankAlgorithms/Algorithms/ReductionSum.cs
@@ -21,7 +21,6 @@ namespace DankAlgorithms.Algorithms
 
         public static int Sum(int[] inputArray)
         {
-            inputArray.Sum();
             return (from x in inputArray.AsParallel() select x).Sum();
         }
     }
