c9f976a5e2fbd95ee25e1012145661dca6ac159f
ToastyTad <toastytadgit@gmail.com>
2018-04-30 20:27:26 -0400

    Added basic async functionality.

--- DankAlgorithms/MainWindow.xaml.cs
+++ DankAlgorithms/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using DankAlgorithms.Utilities;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,6 +14,11 @@ namespace DankAlgorithms
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string RUNNING = "Running";
+        const string COMPLETE = "Complete";
+
+        CancellationTokenSource _cts;
+
         public int DatasetSize { get; set; }
 
         /// <summary>
@@ -39,23 +46,67 @@ namespace DankAlgorithms
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
             Button clickedButton = (Button)sender;
-            switch (clickedButton.Name)
+            Stopwatch sw = new Stopwatch();
+            switch (clickedButton.Name.ToLower())
             {
                 case "quick":
                     break;
                 case "insertion":
+                    iStatus.Text = RUNNING;
+                    iSortRuntime.Content = string.Empty;
+                    sw.Start();
+                    await InsertionSort.SortAsync(RandomArray.GetRandomArray(DatasetSize, 0, 100));
+                    sw.Stop();
+                    iStatus.Text = COMPLETE;
+                    iSortRuntime.Content = sw.Elapsed;
                     break;
                 case "evenodd":
-                    Stopwatch sw = new Stopwatch();
+                    eoStatus.Text = RUNNING;
+                    eoSortRuntime.Content = string.Empty;
                     sw.Start();
-                    EvenOddSort.Sort(RandomArray.GetRandomArray(DatasetSize, 0, 100));
+                    await EvenOddSort.SortAsync(RandomArray.GetRandomArray(DatasetSize, 0, 100));
                     sw.Stop();
+                    eoStatus.Text = COMPLETE;
                     eoSortRuntime.Content = sw.Elapsed;
                     break;
                 case "bogo":
+                    break;
+                case "all":
+                    iSortRuntime.Content = string.Empty;
+                    eoSortRuntime.Content = string.Empty;
+                    allRuntime.Content = string.Empty;
+                    iStatus.Text = RUNNING;
+                    eoStatus.Text = RUNNING;
+                    allStatus.Text = RUNNING;
+                    sw.Start();
+                    Task inSort = InsertionSort.SortAsync(RandomArray.GetRandomArray(DatasetSize, 0, 100));
+                    await inSort.ContinueWith(t =>
+                     {
+                         this.Dispatcher.Invoke(() =>
+                         {
+                             iSortRuntime.Content = sw.Elapsed;
+                             iStatus.Text = COMPLETE;
+                         });
+                     });
+                    Task eoSort = EvenOddSort.SortAsync(RandomArray.GetRandomArray(DatasetSize, 0, 100));
+                    await eoSort.ContinueWith(t =>
+                    {
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            eoSortRuntime.Content = sw.Elapsed;
+                            eoStatus.Text = COMPLETE;
+                        });
+                    });
+                    await Task.WhenAll(inSort, eoSort);
+                    sw.Stop();
+                    allRuntime.Content = sw.Elapsed;
+                    allStatus.Text = COMPLETE;
+                    break;
+                case "cancel":
+                    _cts?.Cancel();
                     break;
                 default:
                     break;
